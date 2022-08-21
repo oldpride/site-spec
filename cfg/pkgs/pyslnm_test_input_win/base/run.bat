736898b7951742e16e98fd8d090dd937e27de167
Tianhua Han <tianhua_han@yahoo.com>
2022-08-21 18:16:41 +0100

    update

--- cfg/pkgs/pyslnm_test_input_win/base/run.bat
+++ cfg/pkgs/pyslnm_test_input_win/base/run.bat
@@ -3,10 +3,12 @@
 set "prog=%~n0"
 set "SITEBASE=%~dp0"
 
+:: use call so that the ending of the command will not end this whole script
 call "%SITEBASE%github\site-spec\cmd_exe\siteenv.cmd"
+
 call p3env
 call svenv
 
 call pyslnm_test_input auto s=henry
 
-call venv
+call dvenv
