77e99162689d687ddcb936d0029a371b04351a6e
Tianhua Han <tianhua_han@yahoo.com>
2023-09-21 13:46:22 +0100

    update

---
+++ python3/scripts/ptbatch_dummy_cfg_batch.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+
+# this is to test "make_cfg_exec.bash all"
+
+from urllib.parse import urlparse
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from pprint import pformat
+import tpsup.dummytools
+import tpsup.pstools
+import tpsup.envtools
+
+our_cfg = {
+    'module': 'tpsup.dummytools',
+
+    'position_args': ['host_port'],
+
+    'extra_args': {
+        'dummyarg1': {'switches': ['-da1', '-dummyarg1'], 'default': False,
+                      'action': 'store_true', 'help': 'dummyarg1 in cfg.py'},
+        'dummyarg3': {'switches': ['-da3', '-dummyarg3'], 'default': False,
+                      'action': 'store_true', 'help': 'dummyarg3 in cfg.py'},
+
+        # overwrite 'record' arg to set default to 'detail'.
+        'record': {'switches': ['--record'],
+                   'default': 'detail',
+                   'action': 'store',
+                   'help': 'record keys (separated by comma) to avoid run twice. default: detail'},
+
+        # extra_args's key 'show_progress' overwrite our_cfg's 'show_progress'.
+        # we need explicity set the default to None,
+        #    so that it will default to our_cfg's 'show_progress'.
+        # (action=store_false's default is True, not None.)
+        # the complete waterfall is:
+        #    extra_args -> our_cfg -> module.py -> batch.py
+        # also the destination of the value goes the extra_args's key 'show_progress'. therefore,
+        # we can name the swiches libraly, here we used '-hide_progress'.
+        'show_progress': {'switches': ['-hide_progress'],
+                          'default': None,  # this effectively default to our_cfg's show_progress
+                          'action': 'store_false',  # overwrite our_cfg's show_progress=1
+                          'help': 'disable show_progess'},
+    },
+
+    'usage_example': '''
+    - this test script doesn't run selenium at all. it just shows how to use cfg.
+    {{prog}} auto s=user
+    {{prog}} auto any
+    {{prog}} auto -b tpbatch_test_batch.txt
+
+    - run again to test record log
+    {{prog}} auto -b tpbatch_test_batch.txt
+
+    - compare show_progress value
+    {{prog}} auto -b tpbatch_test_batch.txt --record ""
+    {{prog}} auto -b tpbatch_test_batch.txt --record "" -hide_progress
+    ''',
+
+    # all keys in keys, suits and aliases (keys and values) will be converted in uppercase
+    # this way so that user can use case-insensitive keys on command line
+    'keys': {
+        'MYNAME': tpsup.envtools.get_user_firstlast(),
+        'CATEGORY': 'Technology Processing',
+        'SUBCATEGORY': 'User Complaint',
+        'SERVICE': 'Trade Plant',
+        'CI': None,
+        'EXTERNAL': 'N',
+        'IMPACT': 3,
+        'URGENCY': 3,
+        'ASSIGNGROUP': 'TradePlantSupport',
+        'ASSIGNTO': None,
+        'SHORT': 'User reported Trade Plant issue',
+        'DETAIL': None,
+    },
+
+    'suits': {
+        'USER': {
+            'CATEGORY': 'Technology Processing',
+            'SUBCATEGORY': 'User Complaint',
+            'SHORT': 'User reported Trade Plant issue',
+        },
+
+        'DATA': {
+            'CATEGORY': 'Data Integrity',
+            'SUBCATEGORY': 'Incorrect Data',
+            'SHORT': 'Incorrect Data in TradePlant',
+        },
+
+        'PROC': {
+            'CATEGORY': 'Technology Faults',
+            'SUBCATEGORY': 'Unexpected Behavior',
+            'SHORT': 'Trade Plant Process failed',
+        },
+
+        'AUTOSYS': {
+            'CATEGORY': 'Technology Faults',
+            'SUBCATEGORY': 'Unexpected Behavior',
+            'SHORT': 'Trade Plant Process failed',
+        },
+    },
+
+    'aliases': {
+        'SH': 'SHORT',
+        'D': 'DETAIL',
+        'I': 'IMPACT',
+        'U': 'URGENCY',
+        'E': 'EXTERNAL',
+        'N': 'MYNAME',
+    },
+
+    'keychains': {
+        # keychains are used to generate a key from other keys. won't cause dead loop.
+        'DETAIL': 'SHORT',
+        'SHORT': 'DETAIL',
+        'ASSIGNTO': 'MYNAME',  # default to self-assigned
+        'CI': 'SERVICE',
+    },
+
+    'show_progress': 1,
+    # "batch_cutoff": 'today',    # midnight
+    # "batch_cutoff": '20300101', # future
+    # 'batch_cutoff': 'today-06', # 6 am
+
+    # where to put the setting?
+    # - settings outside 'opt' are normally used by batch.py's general logic, eg, run_batch().
+    # - settings inside 'opt' are normally used by *_cfg.py's code() function;
+    #   so are command line args.
+    #   use 'opt' if there is a chance that you want to move the setting to command line,
+    #       because this way you don't need to change the batch.py's general logic which
+    #       is likely shared by many scripts.
+    'opt': {
+        # "opt" are optional switches, to be passed into *_cfg.py's code() function's **opt.
+    }
+}
+
+
+def code(all_cfg, known, **opt):
+    # **opt combines all_cfg['opt'] and command line args.
+    verbose = opt.get('verbose', 0)
+    if verbose:
+        print(f'''
+from code(), known =
+{pformat(known)}
+
+from code(), opt =
+{pformat(opt)}
+
+''')
+
+    print(f"show_progress = {all_cfg['show_progress']}")
---
+++ python3/scripts/pttrace_dummy_cfg_trace.py
@@ -0,0 +1,482 @@
+#!/usr/bin/env python3
+
+# this is to test "make_cfg_exec.bash all"
+
+
+from pprint import pformat
+import tpsup.envtools
+import os
+from tpsup.tracer_test_sitespec import update_security_knowledge
+
+# convert above to python
+our_cfg = {
+    # global vars
+
+    # only vars in double curlies {{ }} willl be substituted. this leaves
+    # single curly, like $known{YYYYMMDD}, be eval()'ed.
+    # use array to enforce sequencing because later var may rely on earlier var.
+    'vars': [
+        # resolve a variable now
+        'TPSUP', 'os.environ["TPSUP"]',
+
+        # 'value' is an expression, therefore, needs two different quotes if it is a string.
+        'dummy_global_var2', '"hardcoded string"',
+
+        # use single quote to delay their execution. they will be eval'ed later.
+        'dummy_global_var3', 'get_log_example(known["YYYYMMDD"], known)',
+
+        # refer to a previous var
+        'dummy_global_var4', '"this is a {{dummy_global_var2}}"',
+    ],
+
+    'key_pattern': {
+        # this key is a known's key. if known key is not defined, the pattern
+        # will be used in place
+        'ORDERID': '.+?',
+        'TRADEID': '.+?',
+        'BOOKID': '.+?',
+        'SID': '.+?',
+        'FILLEDQTY': {'pattern': '[0-9.]+'},
+        'TRADEQTY': {'pattern': '[0-9.]+'},
+    },
+
+    'cfg_by_entity': {
+        'test_code': {
+            'method': 'code',
+
+            # method 'code' has no method_cfg as all its implementation
+            # is in 'code' below, which is part of default process
+            'code': '''
+                update_knowledge("DUMMY", "DUM from entity={{entity}}")
+                print(f"access from entity={{ENTITY}} code, known={known}")
+                if 'SECURITY' in known:
+                    print(f"SECURITY={known['SECURITY']}")
+                ''',
+            'AllowZero': 1,
+        },
+
+        'applog_cmd_grep_keys': {
+            'method': 'cmd',
+            'vars': [
+                'log', '"{{cfgdir}}/tptrace_test.log"',
+            ],
+
+            'method_cfg': {
+                'type': 'grep_keys',
+                # if key is in known, use its value to grep.
+                'value': [
+                    'TRADEID',
+                    {'key': 'ORDERID', 'pattern': 'orderid={{opt_value}}'},
+                    'BOOKID',
+                ],
+                'file': '"{{log}}"',
+                'logic': 'AND',
+            },
+            'tail': 5,
+            'tests': [
+                {
+                    'test': 'row_count > 0',
+                    'if_success': 'update_ok(   "seen TRANSACTION in {{log}}");',
+                    'if_failed': 'update_error(  "no TRANSACTION in {{log}}");',
+                },
+            ],
+            'comment': 'test using grep_keys to generate command',
+        },
+
+        'applog_cmd_pipe': {
+            'method': 'cmd',
+            'vars': [
+                'log', '"{{cfgdir}}/tptrace_test.log"',
+            ],
+
+            'method_cfg': {
+                'type': 'pipe',
+                # 2D array,
+                #    outer loop is connected by pipe
+                #    inner loop is for OR logic for grep command.
+                #
+                # method_cfg => {
+                #    type  => 'pipe',
+                #    value => [
+                #       ['grep=grep -E', 'OR11', 'OR12'],
+                #       ['grep=grep -v -E', 'OR21', 'OR22'],
+                #       ['grep=grep -E ',   'OR31', 'OR32'],
+                #       ['cmd=grep -v -E "{{JUNK1=j1}}|{{JUNK2=j2}}"'],
+                #       ['cmd=tail -10'],
+                #    ],
+                #    file => 'app.log',
+                # },
+                #
+                #  if only $known{OR11}, $known{OR12}, $known{OR21} are defined, this will generate
+                #    grep -E '{{OR11}}|{{OR12}}' app.log |grep -v -E '{{OR21}}'|grep -v "j1|j2"|tail -10
+                #
+                #  other value:
+                #      ['pygrepl=pygrepl', 'TRADEID|ORDERID', 'x=BOOKID'],
+
+                'value': [
+                    ['grep=pygrep', 'TRADEID', 'ORDERID'],
+                    ['grep=pygrep -v', 'BOOKID'],
+                    ['cmd=pygrep -v "{{JUNK=junk1}}|{{JUNK=junk2}}"'],
+                ],
+                'file': '{{log}}',
+            },
+            'top': 5,
+            # 'AllowZero': 0,
+            'AllowMultiple': 1,
+            'tests': [
+                {
+                    'test': 'row_count > 0',
+                    'if_success':   'update_ok("pipe: seen TRANSACTION in {{log}}");',
+                    'if_failed': 'update_error("pipe:   no TRANSACTION in {{log}}");',
+                },
+            ],
+
+            'comment': 'test using pipe to generate command',
+        },
+
+        'applog_cmd_post_code': {
+            'method': 'cmd',
+            'vars': [
+                'log', '"{{cfgdir}}/tptrace_test.log"',
+            ],
+
+            'method_cfg': {
+                'type': 'cmd',
+                'value': 'pygrep -E " (ERROR|FAIL)[ :]" {{log}}',
+            },
+
+            'top': 5,
+
+            'post_code': '''
+                if row_count > 0:
+                    update_error("seen ERROR in {{log}}")
+                    print(f"access from entity={{ENTITY}}")
+            ''',
+        },
+
+        'orders': {
+            'method': 'db',
+            'method_cfg': {
+                'db': 'tptest@tpdbmssql',
+                'db_type': 'mssql',
+                # 'db': 'tptest@tpdbmysql',
+                # 'db_type': 'mysql',
+                'where_clause': {
+                    'ORDERID': 'orderid',
+                    'SID': 'sid',
+                    'ORDERQTY': {'numeric': 1, 'clause': "orderqty <= {{opt_value}}"},
+                    'LASTQTY': {'column': 'lastqty', 'numeric': 1},
+                    'FILLEDQTY': {'column': 'filledqty', 'numeric': 1},
+                    'SENDERCOMP': 'SenderComp',
+                    'TARGETCOMP': 'TargetComp',
+                    'YYYYMMDD': {'clause': "CAST(LastUpdateTime as DATE) = '{{opt_value}}'",
+                                 'update_knowledge': 0,
+                                 # update_knowledge is an expression
+                                 # eg. update_knowledge=>'{{new_value}} != 0',
+                                 },
+                },
+                'order_clause': 'order by LastUpdateTime',
+
+                # this only works for today
+                # example_clause => "OrderDate > CAST(GETDATE() as DATE) and Status = 'Filled'",
+                'example_clause': "CAST(LastUpdateTime as DATE) = '{{yyyymmdd}}'",
+            },
+            'comment': "comment from entity={{entity}}.\n",
+        },
+
+        'trades': {
+            'method': 'db',
+            'method_cfg': {
+                'db': 'tptest@tpdbmssql',
+                'db_type': 'mssql',
+                'where_clause': {
+                    # one order can have multiple trades
+                    'TRADEID': 'tradeid',
+                    'ORDERID': 'orderid',
+                    'SID': 'sid',
+                    'ORDERQTY': {'column': 'qty',
+                                 'numeric': 1,
+                                 'clause': "qty <= {{opt_value}}",
+                                 'update_knowledge': 0,
+                                 },
+                    'FILLEDQTY': {'column': 'qty',
+                                  'numeric': 1,
+                                  'clause': "qty <= {{opt_value}}",
+                                  'update_knowledge': 0,
+                                  },
+                    'TRADEQTY': {'column': 'qty', 'numeric': 1, },
+                    'TRADEPRICE':  {'column': 'Price', 'numeric': 1},
+                    'TARGETCOMP': 'TargetComp',
+                    'YYYYMMDD': {'clause': "CAST(TradeDate as DATE) = '{{opt_value}}'",
+                                 'update_knowledge': 0,
+                                 }
+                },
+                'order_clause': 'order by LastUpdateTime',
+                'example_clause': "TradeDate >= '{{yyyy}}{{mm}}{{dd}}'",
+            },
+            'comment': "comment from entity={{entity}}.\n",
+        },
+
+        'actions': {
+            'method': 'db',
+            'method_cfg': {
+                'db': 'tptest@tpdbmssql',
+                'db_type': 'mssql',
+                'where_clause': {
+                    # one order can have multiple trades
+                    'ORDERID': 'msgid',
+                    'FILLEDQTY': {'column': 'filledqty', 'numeric': 1},
+                },
+                # order_clause => 'order by LastUpdateTime',
+                # example_clause => "TradeDate >= '{{yyyy}}{{mm}}{{dd}}'",
+                # Id,MsgId,STATUS,FilledQty
+                # 1,ORD-0001,,
+                # 2,ORD-0001,SENT,
+                # 3,ORD-0001,PARTIAL,600
+                # 4,ORD-0001,COMPLETED,1000
+            },
+            'csv_filters': [
+                {
+                    'condition': '2==1+1',
+                    'ExportExps': [
+                        # use ternary operator to make kv pair.
+                        'weight=0 if r["STATUS"] == "COMPLETED" else 1 if r["STATUS"] == "PARTIAL" else 2',
+                    ],
+                    'SortKeys': ['weight'],
+                },
+            ],
+            'comment': 'trace in trades table',
+            'AllowMultiple': 1,
+            'top': 1,
+            # to test:
+            #     tptrace_test -t actions orderid=ORD-0001
+        },
+
+        'booking': {
+            'method': 'db',
+            'method_cfg': {
+                'db': 'tptest@tpdbmssql',
+                'db_type': 'mssql',
+                'template': """
+                    -- this is on purpose convoluted to show how template is used in complex query
+                    select * from (
+                          select * from booking (nolock) bk
+                            where  1=1
+                                      {{where::YYYYMMDD}}
+                                        {{where::SID}}
+                    ) as BookingByTradeDateSid
+                    where 1=1
+                        {{where::BOOKID}}
+                        {{where::TRADEID}}
+                        {{where::ORDERID}}
+                        {{where::TRADEPRICE}}
+                        {{where::ORDERQTY}}
+                        {{where::TRADEQTY}}
+                        {{where::TARGETCOMP}}
+                    """,
+                'where_clause': {
+                    # one order can have multiple trades
+                    'TRADEID': 'tradeid',
+                    'BOOKID': 'bookid',
+                    'ORDERID': 'orderid',
+                    'ORDERQTY': {'numeric': 1, 'clause': "OrderQty = {{opt_value}}"},
+                    'TRADEQTY': {'column': 'qty', 'numeric': 1, },
+                    'TRADEPRICE':  {'column': 'Price', 'numeric': 1},
+                    'TARGETCOMP': 'TargetComp',
+
+                    # these two has table prefix
+                    'SID': 'bk.sid',
+                    'YYYYMMDD': {'clause': "CAST(bk.TradeDate as DATE) = '{{opt_value}}'",
+                                 'update_knowledge': 0,
+                                 },
+                },
+                'order_clause': 'order by LastUpdateTime',
+                'example_clause': "TradeDate = '{{yyyymmdd}}'",
+            },
+        },
+
+        'applog_log': {
+            'method': 'log',
+            'method_cfg': {
+                'log': '"{{cfgdir}}/tptrace_test.log"',
+                # named groups
+                # https://docs.python.org/3/library/re.html#regular-expression-syntax
+                # https://stackoverflow.com/questions/10059673
+                'extract': 'tradeid=(?P<TRADEID>{{pattern::TRADEID}}),.*?sid=(?P<SID>{{pattern::SID}}),.*?bookid=(?P<BOOKID>{{pattern::BOOKID}}),.*?qty=(?P<TRADEQTY>{{pattern::TRADEQTY}}),',
+            },
+            'update_key': {
+                'BOOKID': 'BOOKID',
+                'TRADEID': 'TRADEID',
+                'SID': 'SID',
+                'TRADEQTY': {'column': 'TRADEQTY', 'numeric': 1},
+            },
+        },
+
+        'applog_section': {
+            'method': 'section',
+            'method_cfg': {
+                'log': '"{{cfgdir}}/tptrace_test_section*.log"',
+
+                # PreMatch/PreExclude are tried before BeginPattern/EndPattern are tried
+                # they are for speedup, it covers every line, therefore, be careful to
+                # avoid filtering out BeginPattern/EndPattern.
+                'PreMatch': '^2021',
+                # PreExclude => '^2022',
+
+                # this cfg will transferred to TPSUP::LOG::get_log_sections() sub
+                'BeginPattern': 'section id .*? started',
+                'EndPattern': 'section completed',
+
+                # PostPattern/PostPattern are tried after BeginPattern/EndPattern are tried
+                # they are also for speed-up
+                'PostMatch': 'order id|trade id',
+                # PostExclude => 'no content',
+
+                'ExtractPatterns': [
+                    '^(?P<BeginTime>.{23}) section id (?P<SectionId>.*?) started',
+                    '^(?P<EndTime>.{23}) section completed',
+                    'order id (?P<OrderId>\S+)',
+                    'trade id (?P<TradeId>\S+)',
+                ],
+                'KeyType': {'OrderId': 'Array', 'TradeId': 'Hash'},
+
+                # use csv_filter below for consistency
+                # ItemMatchExp can use {{...}} vars. this is applied after a whole section is
+                # completed.
+                # 'ItemMatchExp': '"TRD-0002" in r["TradeId"]'
+                # ExcludeExp =>'...',
+
+            },
+
+            'csv_filters': [
+                {
+                    'condition': '1',
+                    'MatchExps': ['"TRD-0002" in r["TradeId"]'],
+                    'ExcludeExps': ['"ORD-0001" in r["OrderId"]'],
+                },
+            ],
+
+            'update_key': {
+                # the key is $known key. The column is the the key of a section
+                'SECTION_BEGINTIME': {'column': 'BeginTime'},
+                'SECTION_ENDTIME': {'column': 'EndTime'},
+                'SECTIONID': {'column': 'SectionId'},
+                'ORDERIDS': {'column': 'OrderId'},
+                'ORDERID2': {'code': 'r["OrderId"][0]', 'condition': 'r["OrderId"]'},
+            },
+
+            'comment': 'test entity={{entity}}',
+        },
+
+
+        'applog_path': {
+            'method': 'path',
+            'method_cfg': {
+                'paths': ['{{cfgdir}}'],
+                'MatchExps': ['"tptrace_test" in r["short"]'],
+                # 'HandleExps': ['$short =~ /tptrace_test/'],    # default to 1
+                # 'RecursiveMax': 2,                         # depth. default to 100
+                'find_ls': 1,
+            },
+            'output_key': 'short',      # use short file name to convert @hashes to %hash1
+            'code': '''
+                short = "tptrace_test_cfg_trace.py"
+                if short in hash1:
+                    mtime_local = hash1[short][0]['mtimel']
+                    if re.search(r'{{YYYY}}{{MM}}{{DD}}', mtime_local):
+                        update_error(f"{short} is updated today {mtime_local}")
+                    else:
+                        update_ok(f"{short} is not updated today {mtime_local}. last update: {hash1[short][0]['mtimel']}")
+                else:
+                    update_error(f"{short} is not found")
+            ''',
+
+            'comment': 'test method=path',
+            'AllowMultiple': 1,
+        },
+    },
+
+    'extra_keys': ['example', 'security', 'YYYYMMDD'],
+
+    'alias_map': {
+        'sec': 'security',
+        'oid': 'orderid',
+        'tid': 'tradeid',
+    },
+
+    'extend_key_map': [
+        # one key's knowledge can extend to other keys.
+        #    for example, if I know SEDOL, I will know CUSIP.
+        # use a array to enforce the order.
+        # extender function, first arg is ref to $known, second arg is new value
+        ['SECURITY', update_security_knowledge],
+        ['SEDOL', update_security_knowledge],
+    ],
+
+    'entry_points': [
+        # use a array to enforce the order
+        # known keys => table list
+        [['BOOKID'], ['booking', 'trades']],
+        [['TRADEID'], ['trades']],
+        [['ORDERID'], ['orders']],
+
+        # [['SID', 'QTY'], ['orders']],
+        # [[], ['orders']],  # this is default
+    ],
+
+    # trace entities in this order
+    'trace_route': [
+        'test_code',
+        {'entity': 'orders', 'condition': '"YYYYMMDD" in known'},
+        {'entity': 'trades', 'AllowZero': 1},
+        'actions',
+        'booking',
+        'applog_cmd_grep_keys',
+        'applog_cmd_pipe',
+        'applog_cmd_post_code',
+        'applog_log',
+        'applog_section',
+        'applog_path',
+    ],
+
+    'usage_example':  '''
+    {{prog}} example=orders yyyymmdd=20211129
+    
+    # test all entities
+    {{prog}} sec=IBM orderqty=4,500 tradeqty=400 yyyymmdd=20211129
+
+    # test actions
+    {{prog}} -t actions orderid=ORD-0001
+
+    # test applog_cmd_grep_keys
+    {{prog}} -t applog_cmd_grep_keys orderid=ORD-0001 tradeid=TRD-0002 bookid=BKG-0002
+
+    # test applog_cmd_grep_keys
+    {{prog}} -t applog_cmd_pipe orderid=ORD-0001 tradeid=TRD-0002 bookid=BKG-0002
+
+    # test applog_cmd_post_code
+    {{prog}} -t applog_cmd_post_code orderid=ORD-0001 
+
+    # test applog_log
+    {{prog}} -t applog_log bookid=BKG-0002 sid=400001 TRADEID=TRD-0002 TRADEQTY=400
+    {{prog}} -t applog_log any
+
+    # test applog_section
+    {{prog}} -t applog_section bookid=BKG-0002 sid=400001 TRADEID=TRD-0002 TRADEQTY=400
+
+    # test applog_path
+    {{prog}} -t applog_path any
+    ''',
+}
+
+# this should be site-spec functions
+
+
+def get_log_example(arg1, known):
+    print(f'get_log_example() get knowledge={pformat(known)}')
+    print(f'get_log_example() arg1={arg1}')
+
+    import os
+    TPSUP = os.environ.get('TPSUP')
+    log = f'{TPSUP}/scripts/tptrace_test.log'
+    return log
